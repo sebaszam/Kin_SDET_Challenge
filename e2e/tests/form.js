40658ed0adcfd59f3db7e4543be45ce4d6fdf28e
Sebastian Zamora <sebastian@wondersauce.com>
2020-11-10 13:22:08 -0600

    Adding more validations
    - Ability to validate data coming from an csv or json
    - Error capture in case json file it's not present
    - Clear values before setting a value
    - Adding dynamic variables
    - Adding a json data.json test file with the records data

--- e2e/tests/form.js
+++ e2e/tests/form.js
@@ -7,7 +7,7 @@ module.exports = {
             .pause(3000)
     },
 
-    '@tags': ['load', 'fill-form', 'submitting-form'],
+    '@tags': ['load', 'fill-form'],
     'Page load': function(browser) {
         browser
             .assert.not.urlEquals('')
@@ -17,27 +17,45 @@ module.exports = {
     },
 
     'Filling up a form': function(browser) {
+        var books;
+
+        try{
+            books = require('../data.json');
+        } catch(err) {
+            console.log(err);
+            console.log ('Couldn\'t load the data.json file. Please ensure that ' +
+                'you have the file in subfolder ../' +
+                'in the same folder as the tests');
+            process.exit();
+        }
+
         const muchReadOpenerSelector = '#time_selector';
-        const muchReadValueSelector = '.bookitem[value="partially"]'; //You can change the value to select here
+        const muchReadValueSelector = '.bookitem[value="' + books.Record2.Read + '"]'; //You can change the value to select here
         const timeReadOpenerSelector = '#status_selector';
         const timeReadValueSelector = '.readtime[value="2_3weeks"]'; //You can change the value to select here
         const ratingOpenerSelector = '#status_selector';
         const ratingValueSelector = '.rating[value="4"]'; //You can change the value to select here
+        const radioValueSelector = '.radioCTA[value="no"]'; //You can change the value to select here
 
         browser
-            .assert.visible('input[name="name"')
-            .setValue('input[name="name"]', 'Test')
+            .assert.visible('input[name="name"]')
+            .clearValue('input[name="name"]')
+            .setValue('input[name="name"]', "Test")
             .pause(1000)
-            .assert.visible('input[name="town_resident"')
-            .setValue('input[name="town_resident"]', 'yes')
-            .assert.visible('input[name="title"')
-            .setValue('input[name="title"]', 'Title Test')
-            .assert.visible('input[name="author"')
-            .setValue('input[name="author"]', 'Author Test')
+            .assert.visible('input[name="town_resident"]')
+            .click(radioValueSelector)
+            .assert.visible('input[name="title"]')
+            .clearValue('input[name="title"]')
+            .setValue('input[name="title"]', books.Record2.Title)
+            .assert.visible('input[name="author"]')
+            .clearValue('input[name="author"]')
+            .setValue('input[name="author"]', books.Record2.Author)
             .assert.visible('input[name="pages"')
-            .setValue('input[name="pages"]', '100')
-            .assert.visible('input[name="dewey_decimal"')
-            .setValue('input[name="dewey_decimal"]', '1234567890')
+            .clearValue('input[name="pages"]')
+            .setValue('input[name="pages"]', books.Record2.Pages)
+            .assert.visible('input[name="dewey_decimal"]')
+            .clearValue('input[name="dewey_decimal"]')
+            .setValue('input[name="dewey_decimal"]', books.Record2.DDC)
             .click(muchReadOpenerSelector)
             .click(muchReadValueSelector)
             .click(timeReadOpenerSelector)
